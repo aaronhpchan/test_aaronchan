418ef142172d5d4063eefcfdf12066c20d6a0acb
aaronhpchan <aaronhpchan@gmail.com>
2022-10-19 06:19:39 -0400

    Update html and js files

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 import json from './data.json' assert { type: 'json' };
 
 const { data } = json;
-console.log(data)
 
 function generateTable() {
   const tbody = document.querySelector('tbody');
-  const company = document.querySelector('.company');
-
+  const table = document.querySelector('table');
+  
   // insert cells
   for (let i = 0; i < data.length; i++) {
     // insert table rows
     const row = document.createElement('tr');
+    const cellNum = Object.values(data[i]).length;
 
-    const cell = document.createElement('tr');
-    const cellText = document.createTextNode(data[i].company_name);
-    cell.appendChild(cellText);
-    cell.style.textTransform = 'capitalize';
-    company.appendChild(cell);
+    for (let j = 0; j < cellNum; j++) {
+      const cell = document.createElement('td');
+      let cellText;
 
+      if (Object.keys(data[i])[j] === 'contact') {
+        let mobileNum = Object.values(data[i])[j].mobile;
+        mobileNum = mobileNum.substring(0, 4) + ' ' + mobileNum.substring(4, mobileNum.length);
+        cellText = document.createTextNode(mobileNum);
+      } else if (Object.keys(data[i])[j] === 'spend_amount') {
+        let amount = `HKD ${Object.values(data[i])[j]}`
+        cellText = document.createTextNode(amount);
+      } else {
+        cellText = document.createTextNode(Object.values(data[i])[j]);
+      }
+      cell.appendChild(cellText);
+      cell.style.textTransform = 'capitalize';
+      row.appendChild(cell);
+    }
+    
     // add the row to the end of the table body
     tbody.appendChild(row);
   }
+
+  // set table border
+  table.setAttribute('border', '1');
 }
 
 generateTable();
